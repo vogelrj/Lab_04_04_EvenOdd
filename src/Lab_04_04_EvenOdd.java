4eee008b4a348ee2bf44bdb24a1370994e887ee2
Rudi <vogelrj@mail.uc.edu>
2022-09-15 21:36:27 -0400

    Initial commit

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,10 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+      int numberToExamine = 6;
+        System.out.println("The starting number is " + numberToExamine);
+      if (numberToExamine%2==0)
+          System.out.println("This number is even.");
+      else
+          System.out.println("This number is odd.");
+    }
+}
